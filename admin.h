d045fd3143f5bba4a6da9a2eab3d5fc72dbbf3f7
Dina272727 <almadinah620@gmail.com>
2025-11-27 22:03:14 +0700

    Add files via upload

---
+++ admin.h
@@ -0,0 +1,10 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include "menu.h"
+#include "pelanggan.h"
+
+// fungsi admin menerima referensi ke list menu & pelanggan
+void menuAdmin(ListMenu &LM, ListPelanggan &LP);
+
+#endif
+
